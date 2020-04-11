9df780ca6a012659667a580265988de6cc5aa4a3
santo1717 <santoshsgore17@gmail.com>
2020-04-12 01:58:51 +0530

    Adding the project

---
+++ src/main/java/com/ecommerce/catalog/CatalogMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ecommerce/catalog/controller/CatalogController.java
@@ -0,0 +1,5 @@
+package com.ecommerce.catalog.controller;
+
+public class CatalogController {
+
+}
